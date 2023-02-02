2cc44b011c0aa3a5ace315762b2828beff6d72a8
Sebastian Ospina <55632072+JSebastianOspina@users.noreply.github.com>
2023-02-02 11:15:18 -0500

    Create navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,15 @@
-
+import logo from './assets/instagram.png';
 export const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <nav class="navbar bg-dark" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src={logo} alt="Logo" width="28" height="28" class="d-inline-block align-text-center" />
+                   <span className="ms-2">
+                    Instagram Story Downloader
+                   </span>
+                </a>
+            </div>
+        </nav>
+    )
 }
